4ef0f37a32a7460430db1bc7d11f42d82580b694
Henry Yang <henryyang1@fareastone.com.tw>
2024-01-23 11:23:27 +0800

    Unique Number Of Occurrences

---
+++ UniqueNumberOfOccurrences.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        count = {}
+        for num in arr:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+
+        occurrences = set()
+        for num in count.values():
+            if num in occurrences:
+                return False
+            occurrences.add(num)
+
+        return True
+        
