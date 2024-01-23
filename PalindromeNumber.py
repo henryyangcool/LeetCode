4018bf4e9a09fecc3a5a2a39c3cee15e984c225a
Henry Yang <henryyang1@fareastone.com.tw>
2024-01-23 10:30:55 +0800

    Palindrome Number

---
+++ PalindromeNumber.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        else: 
+            strNum = str(x)
+            strNumLen = len(strNum)
+            middle = int(strNumLen / 2)
+            front = []
+            for i in range(0, middle):
+                front.append(strNum[i])
+            b = strNumLen - 1
+            for i in range(0, len(front)):
+                if front[i] != strNum[b]:
+                    return False
+                b = b - 1
+            return True
+
+a = Solution().isPalindrome(232)
+print(a)
