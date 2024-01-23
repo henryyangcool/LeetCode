93d7388ae61ca8c0e5a9013ae890b8fab73a60d1
Henry Yang <henryyang1@fareastone.com.tw>
2024-01-23 10:21:22 +0800

    Optimize Code

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -1,45 +1,24 @@
 from math import pow
 class Solution:
     def reverse(self, x: int) -> int:
-        if x == 0:
-            return 0
-        elif x < 10 and x > 0:
+        if x < 10 and x >= 0:
             return x
         elif x > 0:
-            count = 0
-            total = 0
-            x_temp = x
-            while x_temp >= 1:
-                x_temp /= 10
-                count += 1
-            x_temp = x
-            count_temp = count - 1
-            for i in range(count):
-                total = (x_temp % 10) * pow(10,count_temp) + total
-                x_temp /= 10
-                x_temp = int(x_temp)
-                count_temp -= 1
-            if int(total) >= int(pow(2,31) - 1):
+            ans = ''
+            x_temp = reversed(str(x))
+            for i in x_temp:
+                ans += i
+            if int(ans) >= int(pow(2,31) - 1):
                 return 0
             else:
-                return int(total)
-        elif x < 0:
+                return int(ans)
+        elif x < 0:   
             x = abs(x)
-            count = 0
-            total = 0
-            x_temp = x
-            while x_temp >= 1:
-                x_temp /= 10
-                count += 1
-            x_temp = x
-            count_temp = count - 1
-            for i in range(count):
-                total = (x_temp % 10) * pow(10,count_temp) + total
-                x_temp /= 10
-                x_temp = int(x_temp)
-                count_temp -= 1
-            total = int(total - 2 * total)
-            if int(total) <= -2147483648:
+            ans = ''
+            x_temp = reversed(str(x))
+            for i in x_temp:
+                ans += i
+            if -int(ans) <= -int(pow(2,31)):
                 return 0
             else:
-                return total
+                return int('-' + str(ans))
