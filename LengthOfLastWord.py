08bd21e34d17ecf151ad4ec9ab3306dbbc5ed29a
Henry Yang <henryyang1@fareastone.com.tw>
2024-01-23 11:50:10 +0800

    Length Of Last World

---
+++ LengthOfLastWord.py
@@ -0,0 +1,6 @@
+class Solution(object):
+        def lengthOfLastWord(self, s):
+            listOfString = s.split(' ')
+            for i in listOfString[::-1]:
+                if (i != ''):
+                    return len(i)
